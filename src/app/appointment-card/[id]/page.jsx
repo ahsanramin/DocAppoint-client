11aeea29f30b936cf2f481623b5e7f757b221e48
ahsanramin <ahsanramin25@gmail.com>
2026-05-23 16:17:40 +0600

    my 26th commit

---
+++ src/app/appointment-card/[id]/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AppointmentCard from './AppointmentCard';
+
+
+export const metadata = {
+    title: "Confirm Appointment | DocAppoint",
+    
+};
+
+
+
+
+const AppointmentPage = () => {
+    return (
+        <div>
+            <AppointmentCard></AppointmentCard>
+        </div>
+    );
+};
+
+export default AppointmentPage;
