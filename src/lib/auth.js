33f1379c8c45f2abc64d10db26e0ee6c139107b6
ahsanramin <ahsanramin25@gmail.com>
2026-05-23 16:11:40 +0600

    my 19th commit

---
+++ src/lib/auth.js
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
+const db = client.db("doctor-appoinment");
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    
+    client
+  }),
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
+  session:{
+    cookieCache: {
+        enabled:true,
+        strategy: "jwt",
+        maxAge: 5 * 24 * 60 * 60 // in second
+    }
+  },
+  plugins: [
+        jwt(), 
+    ],
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
+});
